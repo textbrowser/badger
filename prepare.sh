17c79ea2db2783c04b9b0218aa0bf1912a23d83a
textbrowser <textbrowser@gmail.com>
2023-11-16 08:38:33 -0500

    Prepare.

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+# Alexis Megas.
 
 NAME=$1
 VERSION=$2
@@ -15,3 +16,9 @@ done
 for file in Distributions/build*; do
     sed -i "s/$NAME-.*_/$NAME-$VERSION\_/" $file
 done
+
+file=badger.pro
+
+sed -i "s/QMAKE_CLEAN += Badger/QMAKE_CLEAN += $NAME/" $file
+sed -i "s/PROJECTNAME = Badger/PROJECTNAME = $NAME/" $file
+sed -i "s/TARGET = Badger/TARGET = $NAME/" $file
