fe6c5a45abde46afa8350d5bd665380019eb2f34
textbrowser <textbrowser@gmail.com>
2023-11-16 09:48:13 -0500

    Prepare.

--- badger.sh
+++ badger.sh
@@ -8,12 +8,12 @@ export AA_USEHIGHDPIPIXMAPS=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_X11_NO_MITSHM=1
 
-./Badger --account-image ./Images/Ubuntu/22.04/ubuntu-account.png \
-	 --account-image-size 64,64 \
-	 --background ./Images/Ubuntu/22.04/image_1.jpg \
-	 --background-color "#5a6773" \
-	 --date-time-format "MMM d hh:mm" \
-	 --enable-shortcuts \
-	 --logo-image ./Images/Ubuntu/22.04/ubuntu-logo-icon.png \
-	 --output /tmp/nvidia-modeset \
-	 --show-date-time
+/opt/badger/NAME --account-image ./Images/Ubuntu/22.04/ubuntu-account.png \
+		 --account-image-size 64,64 \
+		 --background ./Images/Ubuntu/22.04/image_1.jpg \
+		 --background-color "#5a6773" \
+		 --date-time-format "MMM d hh:mm" \
+		 --enable-shortcuts \
+		 --logo-image ./Images/Ubuntu/22.04/ubuntu-logo-icon.png \
+		 --output /tmp/NAME \
+		 --show-date-time
