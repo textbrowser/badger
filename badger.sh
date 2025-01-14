a29491dd2bb61d80b6fa19b5a0708d20225ace2e
textbrowser <textbrowser@gmail.com>
2025-01-14 13:59:35 -0500

    Verify program.

--- badger.sh
+++ badger.sh
@@ -15,13 +15,19 @@ then
     name=./NAME
 fi
 
-$name --account-image ./Images/Ubuntu/22.04/ubuntu-account.png \
-      --account-image-size 64,64 \
-      --background ./Images/Ubuntu/22.04/image_1.jpg \
-      --background-color "#5a6773" \
-      --date-time-format "MMM d hh:mm" \
-      --enable-shortcuts \
-      --logo-image ./Images/Ubuntu/22.04/ubuntu-logo-icon.png \
-      --output /tmp/NAME \
-      --show-date-time \
-      --test
+if [ -x $name ]
+then
+    $name --account-image ./Images/Ubuntu/22.04/ubuntu-account.png \
+	  --account-image-size 64,64 \
+	  --background ./Images/Ubuntu/22.04/image_1.jpg \
+	  --background-color "#5a6773" \
+	  --date-time-format "MMM d hh:mm" \
+	  --enable-shortcuts \
+	  --logo-image ./Images/Ubuntu/22.04/ubuntu-logo-icon.png \
+	  --output /tmp/NAME \
+	  --show-date-time \
+	  --test
+else
+    echo "$name not found."
+    exit 1
+fi
